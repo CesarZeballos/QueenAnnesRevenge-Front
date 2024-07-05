23f086d6ad1ce7332274666002adf9f83a2cd375
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-07-05 23:59:01 +0200

    creacion de la page recipes
    en proceso de la creacion de la page.
    creacion del searchbar, y del state para el renderizado de recipes

--- front/src/app/recipes/page.tsx
+++ front/src/app/recipes/page.tsx
@@ -1,7 +1,34 @@
-const Recipes = () => {
+'use client'
+import ButtonIcon from "@/components/buttonIcon";
+import DashboardContainer from "@/components/dashboardContainer";
+import ItemContainer from "@/components/itemContainer";
+import { AddCircle } from "@mui/icons-material";
+import { useState } from "react";
+
+
+const Recipes: React.FC = () => {
+    const [section, setSection] = useState("actives");
+
     return (
         <div className="marginContainer">
-            <h1 className="header1">recipes</h1>
+            <DashboardContainer>
+                <ItemContainer>
+                    <button className="buttonSecondary" onClick={event => setSection("actives")}>Recipes actives</button>
+                    <button className="buttonSecondary" onClick={event => setSection("inactives")}>Recipes incatives</button>
+                    <ButtonIcon href="/newRecipe">
+                        <AddCircle/>
+                        <p>New recipe</p>
+                    </ButtonIcon> 
+                </ItemContainer>
+
+                <ItemContainer>
+                    <h2 className="header2">Recipes {section}</h2>
+                </ItemContainer>
+
+                <ItemContainer className="col-span-2">
+                    <h2 className="header2">Recipe</h2>
+                </ItemContainer>
+            </DashboardContainer>
         </div>
     )
 }
---
+++ front/src/interfaces/interfaceProps.ts
@@ -0,0 +1,5 @@
+export interface IProps {
+    children: React.ReactNode;
+    className?: string;
+    href?: string
+}
