66455554166fc67d3719e7f60b88acfc2fe1f234
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-07-07 15:36:22 +0200

    Update page.tsx

--- front/src/app/recipes/page.tsx
+++ front/src/app/recipes/page.tsx
@@ -3,6 +3,7 @@ import { getRecipes } from "@/utils/fecthRecipes";
 
 const Recipes: React.FC = async () => {
     const recipesData = await getRecipes();
+
     return (
         <>
             <DashboardRecipes recipesData={recipesData}/>
